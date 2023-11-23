d3a8b9ded75999a816f135149d3e63d4007c0483
Ali Ibrahim <allosheribraheem38@gmail.com>
2023-11-23 17:02:21 +0400

    Define Post model.

---
+++ models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+  tags: { type: [String] },
+  createdAt: { type: Date, default: Date.now },
+  LastUpdatedAt: { type: Date, default: Date.now },
+});
+
+PostSchema.pre("save", (next) => {
+  now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+
+  next();
+});
+
+PostSchema.pre("update", (next) => {
+  this.nowLastUpdatedAt = new Date();
+  next();
+});
+
+module.exports = mongoose.model("Post", PostSchema);
