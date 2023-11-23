a4011d9ce1b7309a5a41d3d967a6f21e8067cfb7
Ali Ibrahim <allosheribraheem38@gmail.com>
2023-11-23 15:17:27 +0400

    Initialize the project.

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const index = require("./routes/index");
+const users = require("./routes/users");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", jade);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/", index);
+app.use("/users", users);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err: {}
+
+    res.status(err.status || 500)
+    res.send('error')
+});
+
+module.exports = app
